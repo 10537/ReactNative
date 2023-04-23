e2d1ee6382cae1ef1dab74394607c812c0c331d7
José Moreno <jmoreno@yabok-tech.io>
2023-04-22 22:34:36 -0500

    [IMP]Exercices for understand flex on ReactNative.

--- 02-MiPrimeraApp/App.tsx
+++ 02-MiPrimeraApp/App.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native';
-import { DimensionesScreen } from './src/screens/DimensionesScreen';
+import { TareaScreen } from './src/screens/TareaScreen';
+// import { DimensionesScreen } from './src/screens/DimensionesScreen';
 // import { ContadorScreen } from './src/screens/ContadorScreen';
 // import { BoxObjectModelScreen } from './src/screens/BoxObjectModelScreen';
 // import {HolaMundoScreen} from './src/screens/HolaMundoScreen';
+// import { PositionScreen } from './src/screens/PositionScreen';
+// import { FlexScreen } from './src/screens/FlexScreen';
+
 
 export const App = () => {
   return (
-    <SafeAreaView>
-      <DimensionesScreen/>
+    <SafeAreaView style={{ flex: 1 }}>
+      <TareaScreen/>
     </SafeAreaView>
   );
 };
--- 02-MiPrimeraApp/src/screens/DimensionesScreen.tsx
+++ 02-MiPrimeraApp/src/screens/DimensionesScreen.tsx
@@ -1,25 +1,41 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { Dimensions } from 'react-native';
+
+const { width, height } = Dimensions.get('window');
 
 export const DimensionesScreen = () => {
   return (
     <View>
-        <View style= { styles.cajaMorada }/>
-        <View style= { styles.cajaNaranja } />
+      <View style={ styles.container }>
+          <View style={{ ...styles.cajaMorada,
+          width: width * 0.6  }}/>
+          <View style= { styles.cajaNaranja } />
+      </View>
+      <Text style={ styles.title }>W: {width} H: {height}</Text>
     </View>
   );
 };
 
 
 const styles = StyleSheet.create({
+    container: {
+      width: '100%',
+      height: 600,
+      backgroundColor: 'red',
+    },
     cajaMorada: {
-        width: 100,
-        height: 100,
+        width: '50%',
+        height: '50%',
         backgroundColor: '#5856D6',
     },
     cajaNaranja: {
-        width: 100,
-        height: 100,
+        // width: 100,
+        // height: 100,
         backgroundColor: '#F0A23B',
+    },
+    title: {
+      fontSize: 30,
+      fontWeight: 'bold',
     },
 });
